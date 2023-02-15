3506b1efcbcba203212db681574f51661978e6c1
Hailemelekot <110323716+Hailemelekotmelakie@users.noreply.github.com>
2023-02-15 09:04:20 +0300

    auto

--- src/AppHeader/AppHeader.vue
+++ src/AppHeader/AppHeader.vue
@@ -7,14 +7,29 @@
   <router-link to="/"> Home </router-link>
   <router-link to="/contact"> Telegram </router-link>
   <router-view />
+
+  <div v-for="one in data" :key="one">{{ one.id }}</div>
 </template>
 
 <script>
 import ThemeMixin from "../_mixins/ThemeMixin.vue";
+import Axios from "axios";
 
 export default {
   name: "AppHeader",
   mixins: [ThemeMixin],
+  data() {
+    return {
+      data: null,
+    };
+  },
+  mounted() {
+    Axios.get("https://dormapi.huliesira.com/user")
+      .then((result) => {
+        this.data = result.data;
+      })
+      .catch((e) => console.error(e));
+  },
 };
 </script>
 <style scoped src="@/css/menu.css"></style>
