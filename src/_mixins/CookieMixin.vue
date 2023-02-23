715a74cba00e464a9b59a3b1fb0755f8d9ed6e20
Hailemelekot <110323716+Hailemelekotmelakie@users.noreply.github.com>
2023-02-23 09:54:56 +0300

    auto

--- src/_mixins/CookieMixin.vue
+++ src/_mixins/CookieMixin.vue
@@ -13,6 +13,11 @@ export default {
             window.location.reload()
         },
     },
+    mounted() {
+        console.log(this.$cookies.get('DORM'))
+        console.log(this.$cookies.get('DORM'), "dorm")
+        console.log(this.cookies, "cookie")
+    },
     computed: {
         mannon() {
             return localStorage.getItem('z_c_c_v')
